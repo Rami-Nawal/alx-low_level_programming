ba89de2295ad973fd6745dc9e00883fffb0ad61f
Nawal1 <nawal.rami92@gmail.com>
2023-03-17 17:56:36 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+
+	for (n1 = '0' ; n1 <= '9' ; n1++)
+	{
+		for (n2 = '0' ; n2 <= '9' ; n2++)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar(n1);
+				putchar(n2);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
