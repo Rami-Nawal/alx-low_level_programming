70fcfa57ace77e606baf8321f627930f29a1a213
Nawal1 <nawal.rami92@gmail.com>
2023-03-21 12:04:28 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - Entry point
  *
@@ -6,6 +7,8 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char a[] = "_putchar\n";
+
+	write(1, a, 8);
 	return (0);
 }
