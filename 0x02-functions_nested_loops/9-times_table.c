112c9dce5d305d1bc814a9632dd2c55c72563b73
Nawal1 <nawal.rami92@gmail.com>
2023-03-21 19:06:01 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 				_putchar(c + '0');
 			if (b != 0 && c < 10)
 			{
-				_putchar(';');
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(c + '0');
