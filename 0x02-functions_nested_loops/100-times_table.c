748a8a0b03042ef788e1d74e17f819fed1310fe8
Nawal1 <nawal.rami92@gmail.com>
2023-03-22 19:43:56 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,27 +12,26 @@ void print_times_table(int n)
 	{
 		for (i = 0 ; i <= n ; i++)
 		{
-			for (j = 0 ; j <= n ; j++)
+			_putchar('0');
+			for (j = 1 ; j <= n ; j++)
 			{
-				result = i * j;
-				if (result >= 10 && result <= 99)
-				{
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else if (result >= 100)
+				_putchar(',');
+				_putchar(' ');
+				result =  i * j;
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+				if (result >= 100)
 				{
 					_putchar((result / 100) + '0');
 					_putchar(((result / 10) % 10) + '0');
 				}
-				else
-				_putchar(result + '0');
-				if (j != n)
+				else if (result >= 10 && result <= 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar((result / 10) + '0');
 				}
+				_putchar((result % 10) + '0');
 			}
 			_putchar('\n');
 		}
