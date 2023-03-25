8ce0eed9c844bbd1c43b47f1f10833afd7795e69
Nawal1 <nawal.rami92@gmail.com>
2023-03-24 17:13:25 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: intager
+ *
+ */
+
+void print_line(int n)
+{
+	int i = 1;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
