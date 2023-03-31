53e2cb18601c24c249ed1771e19cc58dcb3a25c7
Nawal1 <nawal.rami92@gmail.com>
2023-03-30 18:11:17 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *leet - function that encodes a string into 1337
+ * @str: string
+ * Return: result
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
