e61e614d3a197c99794fd1f15f889e003feed50f
Nawal1 <nawal.rami92@gmail.com>
2023-04-03 18:38:58 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: string
+ * Return: pointer to the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+}
