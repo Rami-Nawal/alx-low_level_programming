c51bc190087924ba86cfba2c4b01f24b0bdaab87
Nawal1 <nawal.rami92@gmail.com>
2023-04-11 18:24:35 -0700

    create arrey

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i;
 
-	if (size == 0)
+	a = malloc(size);
+	if (size == 0 || a == 0)
 	{
 		return (0);
 	}
 	else
 	{
-		a = malloc(size);
 		for (i = 0; i < size; i++)
 			a[i] = c;
 		return (a);
